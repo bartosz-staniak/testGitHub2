613691a466b8ee0f5e6b0cda422b0afccbdd45a4
Bartosz Staniak <bartosz.staniak@gmail.com>
2018-04-30 02:45:01 +0200

    Loop added
    Put as many arguments as you like.

--- src/testGitHub2/testGitHub2.java
+++ src/testGitHub2/testGitHub2.java
@@ -3,7 +3,7 @@ package testGitHub2;
 public class testGitHub2 {
 	public static void main (String[] arguments) {
 		if(arguments.length > 0)
-		System.out.println(arguments[0]);
-		System.out.println(arguments[1]);
+		for (int i = 0; i < arguments.length; i++)
+			System.out.println(arguments[i]);
 	}
 }
