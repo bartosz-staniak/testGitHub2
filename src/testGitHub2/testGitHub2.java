343ec52dde11e3f2ce6fba38e9747b7ed1b00f20
Bartosz Staniak <bartosz.staniak@gmail.com>
2018-04-30 02:25:47 +0200

    Initial commit

---
+++ src/testGitHub2/testGitHub2.java
@@ -0,0 +1,9 @@
+package testGitHub2;
+
+public class testGitHub2 {
+	public static void main (String[] arguments) {
+		if(arguments.length > 0)
+		System.out.println(arguments[0]);
+		System.out.println(arguments[1]);
+	}
+}
